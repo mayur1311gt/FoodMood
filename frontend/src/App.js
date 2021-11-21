6d9c29e9dbbc995b9f77e2efd98fb815402eb946
Prathmesh2001 <prathmeshw1102@gmail.com>
2021-11-21 17:00:19 +0530

    components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,31 @@
+import './CSS/styles.css';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Auth from './components/Auth';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div>Hello world</div>
+    <Router>
+        <Navbar/>
+        <div>
+          <Routes>
+            <Route exact path="/home" element={<Home />}></Route>
+            <Route exact path="/profile" element={<Profile />}></Route>
+            <Route exact path="/" element={<Auth />}></Route>
+            <Route path="/" element={<div>404</div>}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
   );
 }
 
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            Foot
+        </div>
+    )
+}
+
+export default Footer
